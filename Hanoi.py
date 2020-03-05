aeaf515655e3c3a784b6f63941d864af489f10bf
ArmanSattar <armanscsgo@gmail.com>
2020-03-05 23:25:56 +0000

    Fixed bug

--- Hanoi.py
+++ Hanoi.py
@@ -1,6 +1,5 @@
 def HanoiSolver(nTimes, firstRod, auxRod, endRod):
     if nTimes == 1:
-        rodThree.insert(0, rodOne[0]);rodOne.pop(0)
         print(f'Moving disc 1 from {firstRod} to {endRod}')
     else:
         HanoiSolver(nTimes - 1, firstRod, endRod, auxRod)
@@ -8,19 +7,4 @@ def HanoiSolver(nTimes, firstRod, auxRod, endRod):
         HanoiSolver(nTimes - 1, auxRod, firstRod, endRod)
 
 nTimes = int(input('How many disks are there on tower 1? '))
-
-rodOne = []
-rodTwo = []
-rodThree = []
-x = 1
-
-while x < nTimes + 1:
-    rodOne.append(x)
-    x += 1
-for n in rodThree:
-    print(n)
-for n in rodOne:
-    print(n)
-for n in rodTwo:
-    print(n)
 HanoiSolver(nTimes, 'first rod', 'middle rod', 'end rod')
